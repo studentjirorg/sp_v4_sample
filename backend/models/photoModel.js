6394284d4f2783858b313748f8256e02068e2025
damijanaku <149889052+damijanaku@users.noreply.github.com>
2025-04-20 11:18:24 +0200

    SCRUM-30 Komentiranje slik je omogoceno

--- backend/models/photoModel.js
+++ backend/models/photoModel.js
@@ -1,28 +1,46 @@
 var mongoose = require('mongoose');
-var Schema   = mongoose.Schema;
+var Schema = mongoose.Schema;
+
+// Define comment schema
+var commentSchema = new Schema({
+    text: {
+        type: String,
+        required: true
+    },
+    postedBy: {
+        type: Schema.Types.ObjectId,
+        ref: 'user',
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
 
 var photoSchema = new Schema({
-	'name' : String,
-	'path' : String,
-	'postedBy' : {
-	 	type: Schema.Types.ObjectId,
-	 	ref: 'user'
-	},
-	'message' : String,
-	'views' : Number,
-	'likes' : Number,
-	'dislikes': {
-		type: Number,
-		default: 0
-	},
-  'likedBy': [{
+    'name': String,
+    'path': String,
+    'postedBy': {
+        type: Schema.Types.ObjectId,
+        ref: 'user'
+    },
+    'message': String,
+    'views': Number,
+    'likes': Number,
+    'dislikes': {
+        type: Number,
+        default: 0
+    },
+    'likedBy': [{
         type: Schema.Types.ObjectId,
         ref: 'user'
     }],
     'dislikedBy': [{
         type: Schema.Types.ObjectId,
         ref: 'user'
-    }]
+    }],
+    'comments': [commentSchema]
 });
 
 module.exports = mongoose.model('photo', photoSchema);
